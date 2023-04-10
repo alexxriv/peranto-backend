b2ddbd82d2024bc2a02a0d1a18b4303d56abde10
alexxriv <alex.trrzrivera@gmail.com>
2023-04-10 14:25:45 -0600

    alquemy orm, alembic init

--- peranto-api/app/crud/base.py
+++ peranto-api/app/crud/base.py
@@ -16,11 +16,9 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
     def __init__(self, model:Type[ModelType]):
         """
         CRUD object with default methods to Create, Read, Update, Delete (CRUD),
-
-        ** Parameters**
+        **Parameters**
         * `model`: A SQLAlchemy model class
         * `schema`: A Pydantic model (schema) class
-    
         """
 
         self.model = model
@@ -34,7 +32,7 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
 
     def create(self, db:Session, *, obj_in:CreateSchemaType) -> ModelType:
         obj_in_data = jsonable_encoder(obj_in)
-        db_obj = self.model(**obj_in.dict)
+        db_obj = self.model(**obj_in_data)
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
