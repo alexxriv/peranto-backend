815443455319365999c546f3f2fc25af75c8a0d7
alexxriv <alex.trrzrivera@gmail.com>
2023-04-11 13:20:54 -0600

    added important command to prestart sh

--- peranto-api/prestart.sh
+++ peranto-api/prestart.sh
@@ -4,7 +4,11 @@
 python ./app/backend_pre_start.py
 
 # Run migrations
+
+#alembic revision --autogenerate -m "first migration"
+
 alembic upgrade head
+
 
 # Create initial data in DB
 python ./app/initial_data.py
